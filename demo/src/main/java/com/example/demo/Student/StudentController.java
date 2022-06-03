81623473c5edf4d88985e6a4326713706f3ad0a8
Jose Carlos M. Ramirez <al15211330@ite.edu.mx>
2022-06-03 08:20:52 -0700

    Finished
    Spring Boot Demo

--- demo/src/main/java/com/example/demo/Student/StudentController.java
+++ demo/src/main/java/com/example/demo/Student/StudentController.java
@@ -2,10 +2,14 @@ package com.example.demo.Student;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -20,16 +24,31 @@ public class StudentController
         this.studentService = studentService;
     }
 
-	@GetMapping
-	public List<Student> GetStudents()
-	{
+    @GetMapping
+    public List<Student> GetStudents()
+    {
         return studentService.GetStudents();
-	}
+    }
 
     @PostMapping
     public void RegisterNewStudent(@RequestBody Student student)
     {
         studentService.AddNewStudent(student);
+    }
+
+    @DeleteMapping(path = "{studentId}")
+    public void DeleteStudent(@PathVariable("studentId") Long studentId)
+    {
+        studentService.DeleteStudent(studentId);
+    }
+
+    @PutMapping(path = "{studentId}")
+    public void UpdateStudent
+    (       @PathVariable("studentId") Long studentId,
+            @RequestParam(required = false) String name,
+            @RequestParam(required = false) String email)
+    {
+        studentService.UpdateStudent(studentId, name, email);
     }
 
 }
--- demo/src/main/java/com/example/demo/Student/StudentService.java
+++ demo/src/main/java/com/example/demo/Student/StudentService.java
@@ -1,9 +1,11 @@
 package com.example.demo.Student;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class StudentService
@@ -24,7 +26,8 @@ public class StudentService
 
     public void AddNewStudent(Student student)
     {
-        Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
+        Optional<Student> studentOptional =
+                studentRepository.findStudentByEmail(student.getEmail());
 
         if (studentOptional.isPresent())
         {
@@ -32,5 +35,43 @@ public class StudentService
         }
 
         studentRepository.save(student);
+    }
+
+    public void DeleteStudent(Long studentId)
+    {
+        boolean exists = studentRepository.existsById(studentId);
+
+        if (!exists)
+        {
+            throw new IllegalStateException("student with id " + studentId + " does not exists");
+        }
+
+        studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void UpdateStudent(Long studentId, String name, String email)
+    {
+        Student student = studentRepository.
+        findById(studentId).
+        orElseThrow(() -> new IllegalStateException("student with id " + studentId + " does not exists"));
+
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name))
+        {
+            student.setName(name);
+        }
+
+        if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email))
+        {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+
+            if (studentOptional.isPresent())
+            {
+                throw new IllegalStateException("email taken");
+            }
+
+            student.setEmail(email);
+        }
+
     }
 }
